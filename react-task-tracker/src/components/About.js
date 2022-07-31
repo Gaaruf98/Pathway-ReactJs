4d9a023b091188b0cb806fac7e959e5857ebe922
Gaaruf98 <75407078+Gaaruf98@users.noreply.github.com>
2022-07-31 11:10:59 -0400

    added all the files

---
+++ react-task-tracker/src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Task to do list</h4>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
